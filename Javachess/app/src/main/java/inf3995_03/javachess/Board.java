e468c596adc3052551a6a4d6f86be628529e4078
Lester <louis.tant@gmail.com>
2016-11-03 19:17:33 -0400

    board layout

---
+++ Javachess/app/src/main/java/inf3995_03/javachess/Board.java
@@ -0,0 +1,13 @@
+package inf3995_03.javachess;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Board extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.board);
+    }
+}
