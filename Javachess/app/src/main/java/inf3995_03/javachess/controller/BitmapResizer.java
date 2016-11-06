c6cdc47b119425b6419f6536da25a3528bb26f8d
Lester <louis.tant@gmail.com>
2016-11-06 16:30:40 -0500

    Pieces on board with the right size

---
+++ Javachess/app/src/main/java/inf3995_03/javachess/controller/BitmapResizer.java
@@ -0,0 +1,32 @@
+package inf3995_03.javachess.controller;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by lester on 16-11-06.
+ * Based on http://stackoverflow.com/questions/15440647/scaled-bitmap-maintaining-aspect-ratio
+ */
+
+public class BitmapResizer {
+
+    public static Bitmap resize(Bitmap image, int maxWidth, int maxHeight) {
+        if (maxHeight > 0 && maxWidth > 0) {
+            int width = image.getWidth();
+            int height = image.getHeight();
+            float ratioBitmap = (float) width / (float) height;
+            float ratioMax = (float) maxWidth / (float) maxHeight;
+
+            int finalWidth = maxWidth;
+            int finalHeight = maxHeight;
+            if (ratioMax > 1) {
+                finalWidth = (int) ((float)maxHeight * ratioBitmap);
+            } else {
+                finalHeight = (int) ((float)maxWidth / ratioBitmap);
+            }
+            image = Bitmap.createScaledBitmap(image, finalWidth, finalHeight, true);
+            return image;
+        } else {
+            return image;
+        }
+    }
+}
--- Javachess/app/src/main/java/inf3995_03/javachess/model/ChessPiece.java
+++ Javachess/app/src/main/java/inf3995_03/javachess/model/ChessPiece.java
@@ -3,8 +3,12 @@ package inf3995_03.javachess.model;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Map;
 
 import inf3995_03.javachess.R;
+import inf3995_03.javachess.controller.BitmapResizer;
 
 import static inf3995_03.javachess.view.BoardActivity.chessPieceMap;
 
@@ -13,7 +17,7 @@ import static inf3995_03.javachess.view.BoardActivity.chessPieceMap;
  * Based on http://faculty.washington.edu/stepp/courses/2005spring/tcss360/lectures/files/2005-05-25/Piece.java
  */
 
-public class ChessPiece{
+public class ChessPiece extends AppCompatActivity{
     public static final String BLACK = "Black";
     public static final String WHITE = "White";
 
@@ -31,67 +35,65 @@ public class ChessPiece{
 
 
     public ChessPiece(String color, String type, String position, Context context) {
+        if (!(color.equals(BLACK)      || color.equals(WHITE))
+            || !(type.equals(KING) || type.equals(QUEEN)
+            || type.equals(BISHOP) || type.equals(KNIGHT)
+            || type.equals(ROOK)   || type.equals(PAWN)))
         {
-            if (!(color.equals(BLACK) || color.equals(WHITE))
-                    || !(type.equals(KING) || type.equals(QUEEN)
-                    || type.equals(BISHOP) || type.equals(KNIGHT)
-                    || type.equals(ROOK) || type.equals(PAWN))) {
-                throw new IllegalArgumentException();
+            throw new IllegalArgumentException();
+        }
+
+        this.color = color;
+        this.type = type;
+        this.position = position;
+        //Let's hardcode the size to be (36x36)dp for now
+        Bitmap unResizedImage =null;
+
+        if (color.equals(BLACK)) {
+            switch (type) {
+                case KING:
+                    unResizedImage = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_kb);
+                    break;
+                case QUEEN:
+                    unResizedImage = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_qb);
+                    break;
+                case BISHOP:
+                    unResizedImage = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_bb);
+                    break;
+                case KNIGHT:
+                    unResizedImage = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_nb);
+                    break;
+                case ROOK:
+                    unResizedImage = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_rb);
+                    break;
+                case PAWN:
+                    unResizedImage = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_pb);
+                    break;
             }
-
-            this.color = color;
-            this.type = type;
-            this.position = position;
-
-
-            if (color.equals(BLACK)) {
-                switch (type) {
-                    case KING:
-                        this.image = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_kb);
-                        break;
-                    case QUEEN:
-                        this.image = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_qb);
-                        break;
-                    case BISHOP:
-                        this.image = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_bb);
-                        break;
-                    case KNIGHT:
-                        this.image = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_nb);
-                        break;
-                    case ROOK:
-                        this.image = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_rb);
-                        break;
-                    case PAWN:
-                        this.image = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_pb);
-                        break;
-                }
-
-            } else {
-                switch (type) {
-                    case KING:
-                        this.image = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_kw);
-                        break;
-                    case QUEEN:
-                        this.image = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_qw);
-                        break;
-                    case BISHOP:
-                        this.image = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_bw);
-                        break;
-                    case KNIGHT:
-                        this.image = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_nw);
-                        break;
-                    case ROOK:
-                        this.image = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_rw);
-                        break;
-                    case PAWN:
-                        this.image = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_pw);
-                        break;
-                }
-
+        } else {
+            switch (type) {
+                case KING:
+                    unResizedImage = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_kw);
+                    break;
+                case QUEEN:
+                    unResizedImage = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_qw);
+                    break;
+                case BISHOP:
+                    unResizedImage = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_bw);
+                    break;
+                case KNIGHT:
+                    unResizedImage = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_nw);
+                    break;
+                case ROOK:
+                    unResizedImage = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_rw);
+                    break;
+                case PAWN:
+                    unResizedImage = BitmapFactory.decodeResource(context.getResources(), R.drawable.piece_pw);
+                    break;
             }
-
-        } //ChessPiece
-    }
+        }//else
+        this.image = BitmapResizer.resize(unResizedImage,36,36);
+    }//ChessPiece
 
     //Getters
     public String getColor() {
@@ -122,7 +124,8 @@ public class ChessPiece{
     }
 
     public void setImage(Bitmap image) {
-         this.image = image;
+        BitmapResizer.resize(image,36,36);
+        this.image = image;
     }
 
 
